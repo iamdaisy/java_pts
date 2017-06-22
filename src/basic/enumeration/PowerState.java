645915081a7507640570814d23a2a187b4b927b3
iamdaisy <dahyee89@gmail.com>
2017-06-22 20:05:33 +0900

    update_6_22_2

---
+++ src/basic/enumeration/PowerState.java
@@ -0,0 +1,22 @@
+package basic.enumeration;
+
+public enum PowerState {
+	OFF("전원이 꺼졌습니다."),
+	ON("전원이 켜졌습니다."),
+	SUSPEND("먹통상태임");
+	
+	private String description;
+	
+	private PowerState(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	@Override
+	public String toString() {
+		return getDescription();
+	}
+}
